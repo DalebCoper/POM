eb126df4baca4f38dddf6b2d33200df4df69ae0b
DalebCoper <121140794+DalebCoper@users.noreply.github.com>
2023-03-22 17:46:53 +0300

    minor update

--- tests/test_file_downloader.py
+++ tests/test_file_downloader.py
@@ -4,6 +4,6 @@ from playwright.sync_api import Page
 def test_file_downloader(page: Page) -> None:
     page.goto("https://the-internet.herokuapp.com/download")
     with page.expect_download() as download_info:
-        page.get_by_role("link", name="some-file.txt").click()
+        page.locator("#content > div > a:nth-child(14)").click()
     download = download_info.value
     print(download.path())
