b292d37da976dc766fa2d7b29aaf429abc64398d
DalebCoper <121140794+DalebCoper@users.noreply.github.com>
2023-03-27 20:28:22 +0300

    minor update

--- tests/test_basic_auth.py
+++ tests/test_basic_auth.py
@@ -1,13 +1,13 @@
 from playwright.sync_api import Page, expect
 
 
-def test_auth_valid_user(page: Page) -> None:
+def test_auth_positive(page: Page) -> None:
     page.goto("https://admin:admin@the-internet.herokuapp.com/basic_auth")
 
     expect(page.locator("#content p")).to_have_text("Congratulations! You must have the proper credentials.")
 
 
-def test_auth_invalid_user(page: Page) -> None:
+def test_auth_negative(page: Page) -> None:
     page.goto("https://wrongUN:wrongPW@the-internet.herokuapp.com/basic_auth")
 
     expect(page.locator("body")).to_have_text("Not authorized")
