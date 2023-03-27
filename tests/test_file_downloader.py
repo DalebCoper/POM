89a05ecafb94554a741100662a90322f96b8241b
Daniil Pavlov <121140794+DalebCoper@users.noreply.github.com>
2023-03-27 21:26:25 +0300

    Delete tests directory

--- tests/test_file_downloader.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-
-
-def test_file_downloader(page: Page) -> None:
-    page.goto("https://the-internet.herokuapp.com/download")
-    with page.expect_download() as download_info:
-        page.locator("#content > div > a:nth-child(14)").click()
-    download = download_info.value
-    print(download.path())
