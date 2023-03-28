653a104dbdd869463f0af048b0d48c3b004c3c97
DalebCoper <121140794+DalebCoper@users.noreply.github.com>
2023-03-28 21:59:56 +0300

    up

---
+++ tests_page_object_model/exceptions.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import expect
+from pageObjects.Exceptions import Exceptions
+
+
+def test_no_such_element_exception(page) -> None:
+    exceptions_page = Exceptions(page)
+    exceptions_page.navigate()
+    exceptions_page.add_element()
+
+    expect(page.locator("div > #confirmation")).to_have_text("Row 2 was added")
+
+
+def test_element_not_interactable_exception(page) -> None:
+    exceptions_page = Exceptions(page)
+    exceptions_page.navigate()
+    exceptions_page.add_save_element("text to input")
+
+    expect(page.locator("div > #confirmation")).to_have_text("Row 2 was saved")
---
+++ tests_page_object_model/pageObjects/Exceptions.py
@@ -0,0 +1,15 @@
+class Exceptions:
+    def __init__(self, page):
+        self.page = page
+        self._text_to_input = page.locator("#row2 > .input-field")
+
+    def navigate(self):
+        self.page.goto("https://practicetestautomation.com/practice-test-exceptions/")
+
+    def add_element(self):
+        self.page.locator("div > #add_btn").click()
+
+    def add_save_element(self, text):
+        self.page.locator("div > #add_btn").click()
+        self._text_to_input.fill(text)
+        self.page.locator("#row2 > #save_btn").click()
